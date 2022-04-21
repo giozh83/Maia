10de2f05f7ea23dde6091f84b1730fb9656429ea
giozh83 <giozh83@gmail.com>
2022-04-21 11:42:18 -0700

    gio

--- information/index.js
+++ information/index.js
@@ -1,13 +1,13 @@
 let nishani=1;
-document.querySelector('.profile').addEventListener('click', function(){
+document.querySelector('.picture1_child1').addEventListener('click', function(){
     
     if (nishani==0) {
-        document.querySelector('.profile').style.width='70%'
-        document.querySelector('.profile').style.opacity='0.5'
+        document.querySelector('.picture1_child1').style.width='100%'
+        document.querySelector('.picture1_child1').style.opacity='0.5'
         nishani=nishani+1;
     } else {
-       document.querySelector('.profile').style.width='120%'
-       document.querySelector('.profile').style.opacity='1'
+       document.querySelector('.picture1_child1').style.width='200%'
+       document.querySelector('.picture1_child1').style.opacity='1'
        nishani=0
     }
     
