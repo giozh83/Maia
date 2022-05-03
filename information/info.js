23a3742e59b6414cc612927e273366f4861a76c3
giozh83 <giozh83@gmail.com>
2022-05-03 10:21:08 -0700

    maia

---
+++ information/info.js
@@ -0,0 +1,68 @@
+document.querySelector('.button_modal').addEventListener('click',function(){
+    document.querySelector('.modal').style.height=0;
+    document.querySelector('.modal').style.width=0;
+    document.querySelector('.modal').style.opacity=0;
+
+})
+
+document.querySelector('.photo_1').addEventListener('click',function(){
+    
+    document.querySelector('.modal').style.height='90vh';
+    document.querySelector('.modal').style.width='70vw';
+    document.querySelector('.modal').style.opacity=1;
+    document.querySelector('.modal').style.backgroundImage='url(../photo/1.jpg)';   
+})
+
+document.querySelector('.photo_2').addEventListener('click',function(){
+    
+    document.querySelector('.modal').style.height='90vh';
+    document.querySelector('.modal').style.width='70vw';
+    document.querySelector('.modal').style.opacity=1;
+    document.querySelector('.modal').style.backgroundImage='url(../photo/2.jpg)';  
+    
+})
+
+document.querySelector('.photo_3').addEventListener('click',function(){
+    
+    document.querySelector('.modal').style.height='90vh';
+    document.querySelector('.modal').style.width='70vw';
+    document.querySelector('.modal').style.opacity=1;
+    document.querySelector('.modal').style.backgroundImage='url(../photo/3.jpg)';   
+})
+
+document.querySelector('.photo_4').addEventListener('click',function(){
+    
+    document.querySelector('.modal').style.height='90vh';
+    document.querySelector('.modal').style.width='70vw';
+    document.querySelector('.modal').style.opacity=1;
+    document.querySelector('.modal').style.backgroundImage='url(../photo/4.jpg)';  
+    
+})
+
+document.querySelector('.photo_5').addEventListener('click',function(){
+    
+    document.querySelector('.modal').style.height='90vh';
+    document.querySelector('.modal').style.width='70vw';
+    document.querySelector('.modal').style.opacity=1;
+    document.querySelector('.modal').style.backgroundImage='url(../photo/5.jpg)';  
+    
+})
+
+
+document.querySelector('.photo_6').addEventListener('click',function(){
+    
+    document.querySelector('.modal').style.height='90vh';
+    document.querySelector('.modal').style.width='70vw';
+    document.querySelector('.modal').style.opacity=1;
+    document.querySelector('.modal').style.backgroundImage='url(../photo/6.jpg)';  
+    
+})
+
+document.querySelector('.photo_7').addEventListener('click',function(){
+    
+    document.querySelector('.modal').style.height='90vh';
+    document.querySelector('.modal').style.width='70vw';
+    document.querySelector('.modal').style.opacity=1;
+    document.querySelector('.modal').style.backgroundImage='url(../photo/7.jpg)';  
+    
+})
